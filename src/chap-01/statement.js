7f9c7b0d7d3e2fa4958f451c444a19579dfc6146
futantan <luckytantanfu@gmail.com>
2019-06-21 23:07:12 +0800

    Extract Function

--- src/chap-01/statement.js
+++ src/chap-01/statement.js
@@ -1,5 +1,4 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -7,12 +6,19 @@ function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
   function totalVolumeCredits() {
     let volumeCredits = 0;
