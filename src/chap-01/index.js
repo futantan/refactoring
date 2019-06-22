ef7bde7c00422ca73604b14e902537dab4c44316
futantan <luckytantanfu@gmail.com>
2019-06-22 11:42:40 +0800

    add htmlStatement

--- src/chap-01/index.js
+++ src/chap-01/index.js
@@ -1,6 +1,7 @@
-const { statement } = require("./statement");
+const { statement, htmlStatement } = require("./statement");
 const invoices = require("./invoices");
 const plays = require("./plays");
 
 const result = statement(invoices[0], plays);
 console.log(result);
+console.log(htmlStatement(invoices[0], plays));
