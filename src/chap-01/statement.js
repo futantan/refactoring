b64991559e538b40c881102ed349aa70e20e6315
futantan <luckytantanfu@gmail.com>
2019-06-22 11:33:21 +0800

    Replace Loop with Pipeline

--- src/chap-01/statement.js
+++ src/chap-01/statement.js
@@ -51,19 +51,11 @@ function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   function totalVolumeCredits(data) {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
-    return volumeCredits;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
